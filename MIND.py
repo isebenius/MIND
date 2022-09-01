9ffdd44175e91a09d7650d902179c43954e70ed9
isebenius <isaac.sebenius@gmail.com>
2022-09-01 16:07:45 +0100

    update

--- MIND.py
+++ MIND.py
@@ -53,7 +53,7 @@ def compute_MIND(surf_dir, features, parcellation):
 
 	#standardize across the brain for each feature to get each dimension to roughly the same scale.
 	for x in features_used:
-    	vertex_data[x] = (vertex_data[x] - vertex_data[x].mean())/vertex_data[x].std()
+		vertex_data[x] = (vertex_data[x] - vertex_data[x].mean())/vertex_data[x].std()
 
 	#Drop outliers. This drops vertices with an MAD score in ANY of the used features. Can be customized.
 	# z_score_threshhold = 7
